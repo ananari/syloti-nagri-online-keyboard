2dff465d201aea3dbc0bf0b83c94a91276aaec8a
Anais Ahmed <anaismahmed@uchicago.edu>
2020-04-28 10:24:35 -0500

    cleaned up table

--- sylheti.js
+++ sylheti.js
@@ -8,29 +8,29 @@ const sylhetiTable = {
   'O': {"letter": 'ꠅ', "unicode": "U+A805", "ipa": 'o'},
   '=': {"letter": "\ua806", "unicode": "U+A806", "ipa": ''},
   "k": {"letter": "ꠇ", "unicode": "U+A807", "ipa": "k/x"},
-  "kh": {"letter": "ꠈ", "unicode": "U+A808", "ipa": "k/x (plus following high tone)"},
+  "kh": {"letter": "ꠈ", "unicode": "U+A808", "ipa": "k/x", "high": "yes"},
   "g": {"letter": "ꠉ", "unicode": "U+A809", "ipa": 'g'},
-  "gh": {"letter": "ꠊ", "unicode": "U+A80A", "ipa": "g (plus following high tone)"},
+  "gh": {"letter": "ꠊ", "unicode": "U+A80A", "ipa": 'g', "high": "yes"},
   "ng": {"letter": "\ua80b", "unicode": "U+A80B", "ipa": 'ŋ'},
-  "c": {"letter": "ꠌ", "unicode": "U+A80C", "ipa": "s"},
-  "ch": {"letter": "ꠍ", "unicode": "U+A80D", "ipa": "s (plus following high tone)"},
+  "c": {"letter": "ꠌ", "unicode": "U+A80C", "ipa": 's'},
+  "ch": {"letter": "ꠍ", "unicode": "U+A80D", "ipa": 's', "high": "yes"},
   "j": {"letter": "ꠎ", "unicode": "U+A80E", "ipa": "z"},
-  "jh": {"letter": "ꠏ", "unicode": "U+A80F", "ipa": "z (plus following high tone)"},
+  "jh": {"letter": "ꠏ", "unicode": "U+A80F", "ipa": "z", "high": "yes"},
   "T": {"letter": "ꠐ", "unicode": "U+A810", "ipa": "ʈ"},
-  "Th": {"letter": "ꠑ", "unicode": "U+A811", "ipa": "ʈ (plus following high tone)"},
+  "Th": {"letter": "ꠑ", "unicode": "U+A811", "ipa": "ʈ", "high": "yes"},
   "D": {"letter": "ꠒ", "unicode": "U+A812", "ipa": "ɖ"},
-  "Dh": {"letter": "ꠓ", "unicode": "U+A813", "ipa": "ɖ (plus following high tone)"},
+  "Dh": {"letter": "ꠓ", "unicode": "U+A813", "ipa": "ɖ", "high": "yes"},
   "t": {"letter": "ꠔ", "unicode": "U+A814", "ipa": "t"},
-  "th": {"letter": "ꠕ", "unicode": "U+A815", "ipa": "t (plus following high tone)"},
+  "th": {"letter": "ꠕ", "unicode": "U+A815", "ipa": "t", "high": "yes"},
   "d": {"letter": "ꠖ", "unicode": "U+A816", "ipa": "d"},
-  "dh": {"letter": "ꠗ", "unicode": "U+A817", "ipa": "d (plus following high tone)"},
+  "dh": {"letter": "ꠗ", "unicode": "U+A817", "ipa": "d", "high": "yes"},
   "n": {"letter": "ꠘ", "unicode": "U+A818", "ipa": "n"},
   "p": {"letter": "ꠙ", "unicode": "U+A819", "ipa": "p/f"},
-  "f": {"letter": "ꠚ", "unicode": "U+A81A", "ipa": "p/f (plus following high tone)"},
-  "ph": {"letter": "ꠚ", "unicode": "U+A81A", "ipa": "p/f (plus following high tone)"},
+  "f": {"letter": "ꠚ", "unicode": "U+A81A", "ipa": "p/f", "high": "yes"},
+  "ph": {"letter": "ꠚ", "unicode": "U+A81A", "ipa": "p/f", "high": "yes"},
   "b": {"letter": "ꠛ", "unicode": "U+A81B", "ipa": "b"},
-  "v": {"letter": "ꠜ", "unicode": "U+A81C", "ipa": "b (plus following high tone)"},
-  "bh": {"letter": "ꠜ", "unicode": "U+A81C", "ipa": "b (plus following high tone)"},
+  "v": {"letter": "ꠜ", "unicode": "U+A81C", "ipa": "b", "high": "yes"},
+  "bh": {"letter": "ꠜ", "unicode": "U+A81C", "ipa": "b", "high": "yes"},
   "m": {"letter": "ꠝ", "unicode": "U+A81D", "ipa": "m"},
   "r": {"letter": "ꠞ", "unicode": "U+A81E", "ipa": "r"},
   "l": {"letter": "ꠟ", "unicode": "U+A81F", "ipa": "l"},
@@ -97,13 +97,16 @@ for(const row in sylhetiTable){
   td2 = document.createElement('td');
   td3 = document.createElement('td');
   td4 = document.createElement('td');
-  td1.innerText = sylhetiTable[row].letter;
-  td2.innerText = row;
+  td5 = document.createElement('td');
+  td1.innerText = row;
+  td2.innerText = sylhetiTable[row].letter;
   td3.innerText = sylhetiTable[row].unicode;
   td4.innerText = sylhetiTable[row].ipa;
+  td5.innerText = sylhetiTable[row].high || "no";
   tr.appendChild(td1);
   tr.appendChild(td2);
   tr.appendChild(td3);
-  tr.appendChild(td4)
+  tr.appendChild(td4);
+  tr.appendChild(td5);
   letterTable.appendChild(tr);
 }
